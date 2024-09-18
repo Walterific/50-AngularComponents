2da4f08289c4fd1df705c9f15e58d6ce62c7f9d3
Walter Mark B. Inductivo <inductivo.waltermark.02162002@gmail.com>
2024-09-18 22:32:27 +0800

    #11evenoddchecker

---
+++ src/app/components/evenoddchecker/evenoddchecker.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-evenoddchecker',
+  templateUrl: './evenoddchecker.component.html',
+  styleUrl: './evenoddchecker.component.css'
+})
+export class EvenoddcheckerComponent {
+  number: number | null = null;
+  result: string = '';
+
+  checkEvenOdd(): void {
+    if (this.number !== null) {
+      if (this.number % 2 === 0) {
+        this.result = 'The number is Even';
+      } else {
+        this.result = 'The number is Odd';
+      }
+    } else {
+      this.result = 'Please enter a valid number';
+    }
+  }
+}
