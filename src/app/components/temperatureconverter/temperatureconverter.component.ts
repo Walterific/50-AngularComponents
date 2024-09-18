5ad2540e9317388e55641e86d250e03fd5c6916d
Walter Mark B. Inductivo <inductivo.waltermark.02162002@gmail.com>
2024-09-18 22:43:06 +0800

    #21temperatureconverter

---
+++ src/app/components/temperatureconverter/temperatureconverter.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-temperatureconverter',
+  templateUrl: './temperatureconverter.component.html',
+  styleUrl: './temperatureconverter.component.css'
+})
+export class TemperatureconverterComponent {
+  celsius: number | null = null;
+  fahrenheit: number | null = null;
+
+  convertToCelsius(): void {
+    if (this.fahrenheit !== null) {
+      this.celsius = (this.fahrenheit - 32) * 5 / 9;
+    }
+  }
+
+  convertToFahrenheit(): void {
+    if (this.celsius !== null) {
+      this.fahrenheit = (this.celsius * 9 / 5) + 32;
+    }
+  }
+}
