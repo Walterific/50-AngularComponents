45f15710edf7aae223af7c3a2c6d2950025b5098
Walter Mark B. Inductivo <inductivo.waltermark.02162002@gmail.com>
2024-09-18 22:25:57 +0800

    #2showhellobutton

---
+++ src/app/components/showhellobutton/showhellobutton.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-showhellobutton',
+  templateUrl: './showhellobutton.component.html',
+  styleUrl: './showhellobutton.component.css'
+})
+export class ShowhellobuttonComponent {
+  message: string = '';
+
+  displayHelloWorld() {
+    this.message = 'Hello World!';
+  }
+}
