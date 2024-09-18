1fb437046ae64ef1ec3786f8052ada4b514a0fd5
Walter Mark B. Inductivo <inductivo.waltermark.02162002@gmail.com>
2024-09-18 22:51:47 +0800

    #33interestcalculator

---
+++ src/app/components/interestcalculator/interestcalculator.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-interestcalculator',
+  templateUrl: './interestcalculator.component.html',
+  styleUrl: './interestcalculator.component.css'
+})
+export class InterestcalculatorComponent {
+  principal: number | null = null;
+  rate: number | null = null;
+  time: number | null = null;
+  interest: number | null = null;
+
+  calculateInterest(): void {
+    if (this.principal !== null && this.rate !== null && this.time !== null) {
+      this.interest = (this.principal * this.rate * this.time) / 100;
+    } else {
+      this.interest = null;
+    }
+  }
+}
