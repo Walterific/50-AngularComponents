5a1cc1602fb915a8386223454fb0b12813e6fd3b
Walter Mark B. Inductivo <inductivo.waltermark.02162002@gmail.com>
2024-09-18 22:47:28 +0800

    #27randomnumbergenerator

---
+++ src/app/components/randomnumbergenerator/randomnumbergenerator.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-randomnumbergenerator',
+  templateUrl: './randomnumbergenerator.component.html',
+  styleUrl: './randomnumbergenerator.component.css'
+})
+export class RandomnumbergeneratorComponent {
+  min: number = 1;
+  max: number = 100;
+  randomNumber: number | null = null;
+
+  generateRandomNumber(): void {
+    if (this.min < this.max) {
+      this.randomNumber = Math.floor(Math.random() * (this.max - this.min + 1)) + this.min;
+    }
+  }
+}
