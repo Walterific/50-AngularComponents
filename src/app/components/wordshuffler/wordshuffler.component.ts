484cf801a5f481810371c49242e853b458b9c758
Walter Mark B. Inductivo <inductivo.waltermark.02162002@gmail.com>
2024-09-18 22:49:14 +0800

    #30wordshuffler

---
+++ src/app/components/wordshuffler/wordshuffler.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wordshuffler',
+  templateUrl: './wordshuffler.component.html',
+  styleUrl: './wordshuffler.component.css'
+})
+export class WordshufflerComponent {
+  inputWord: string = '';
+  shuffledWord: string = '';
+
+  shuffleWord(): void {
+    if (this.inputWord) {
+      this.shuffledWord = this.shuffle(this.inputWord);
+    }
+  }
+
+  private shuffle(word: string): string {
+    const array = word.split('');
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array.join('');
+  }
+}
