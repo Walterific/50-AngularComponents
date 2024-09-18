7a91493745dda8e44e7d6163db89f4ba3f8841e6
Walter Mark B. Inductivo <inductivo.waltermark.02162002@gmail.com>
2024-09-18 22:52:19 +0800

    #34compoundinterestcalculator

---
+++ src/app/components/compoundinterestcalculator/compoundinterestcalculator.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-compoundinterestcalculator',
+  templateUrl: './compoundinterestcalculator.component.html',
+  styleUrl: './compoundinterestcalculator.component.css'
+})
+export class CompoundinterestcalculatorComponent {
+  principal: number | null = null;
+  rate: number | null = null;
+  time: number | null = null;
+  frequency: number | null = null;
+  compoundInterest: number | null = null;
+
+  calculateCompoundInterest(): void {
+    if (this.principal !== null && this.rate !== null && this.time !== null && this.frequency !== null) {
+      const r = this.rate / 100;
+      const n = this.frequency;
+      const t = this.time;
+
+      const amount = this.principal * Math.pow((1 + r / n), n * t);
+      this.compoundInterest = amount - this.principal;
+    } else {
+      this.compoundInterest = null;
+    }
+  }
+}
