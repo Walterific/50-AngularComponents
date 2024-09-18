3aa8164662e0091f203c8c68f1627943e455eba7
Walter Mark B. Inductivo <inductivo.waltermark.02162002@gmail.com>
2024-09-18 22:31:10 +0800

    #9textlength

---
+++ src/app/components/textlength/textlength.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-textlength',
+  templateUrl: './textlength.component.html',
+  styleUrl: './textlength.component.css'
+})
+export class TextlengthComponent {
+  inputText: string = '';
+  textLength: number | null = null;
+
+  calculateLength(): void {
+    this.textLength = this.inputText.length;
+  }
+}
