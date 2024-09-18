b50b48cd2d94b946fe44e45852e4994bb3b3b1fb
Walter Mark B. Inductivo <inductivo.waltermark.02162002@gmail.com>
2024-09-18 22:54:15 +0800

    #38randomquotedisplay

---
+++ src/app/components/randomquotedisplay/randomquotedisplay.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-randomquotedisplay',
+  templateUrl: './randomquotedisplay.component.html',
+  styleUrl: './randomquotedisplay.component.css'
+})
+export class RandomquotedisplayComponent {
+  quotes: string[] = [
+    'Be yourself; everyone else is already taken.',
+    'I\'m selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can\'t handle me at my worst, then you sure as hell don\'t deserve me at my best.',
+    'So many books, so little time.',
+    'Two things are infinite: the universe and human stupidity; and I\'m not sure about the universe.',
+    'A room without books is like a body without a soul.'
+  ];
+
+  randomQuote: string = '';
+
+  displayRandomQuote(): void {
+    const randomIndex = Math.floor(Math.random() * this.quotes.length);
+    this.randomQuote = this.quotes[randomIndex];
+  }
+}
