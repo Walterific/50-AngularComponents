fdeb43fc3fc1823f058082e4e3e8a8e5913711f8
Walter Mark B. Inductivo <inductivo.waltermark.02162002@gmail.com>
2024-09-18 22:29:34 +0800

    #6userage

---
+++ src/app/components/userage/userage.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-userage',
+  templateUrl: './userage.component.html',
+  styleUrl: './userage.component.css'
+})
+export class UserageComponent {
+  birthYear: number | null = null;
+  age: number | null = null;
+
+  calculateAge(): void {
+    if (this.birthYear) {
+      const currentYear = new Date().getFullYear();
+      this.age = currentYear - this.birthYear;
+    } else {
+      this.age = null;
+    }
+  }
+}
