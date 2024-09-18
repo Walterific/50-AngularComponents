ea313e90538a6af796d3074e745ac4cfca3e0028
Walter Mark B. Inductivo <inductivo.waltermark.02162002@gmail.com>
2024-09-18 22:37:58 +0800

    #20palindromechecker

---
+++ src/app/components/palindromechecker/palindromechecker.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-palindromechecker',
+  templateUrl: './palindromechecker.component.html',
+  styleUrl: './palindromechecker.component.css'
+})
+export class PalindromecheckerComponent {
+  inputWord: string = '';
+  isPalindrome: boolean | null = null;
+
+  checkPalindrome(): void {
+    const normalizedWord = this.inputWord.toLowerCase().replace(/[^a-z0-9]/gi, '');
+    this.isPalindrome = normalizedWord === normalizedWord.split('').reverse().join('');
+  }
+}
